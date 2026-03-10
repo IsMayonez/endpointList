fd64648cea1269f3f18c90b96c673a30f1210989
Aiman El Sewisy <aiman.sewisy@gmail.com>
2026-03-10 11:39:11 +0100

    shuffff

--- exampleEndpoint.php
+++ exampleEndpoint.php
@@ -1,12 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
+$status="success";
+$message="Works!";
 $minfactor = 0.5;
 $maxfactor = 1.5;
 
 if(isset($_GET['minRandFactor']) && isset($_GET['maxRandFactor'])) {
-    $param1 = $_GET['minRandFactor'];
-    $param2 = $_GET['maxRandFactor'];
+    $minfactor = $_GET['minRandFactor'];
+    $maxfactor = $_GET['maxRandFactor'];
 }
     
 $timemessage = 0;
@@ -17,11 +19,14 @@ if(isset($_GET['duration'])) {
     usleep((int)($duration * 1000000));
     $timemessage = $duration;
     
+} else {
+    $status="useless";
+    $message="You didn't send a duration!";
 }
     
 $response = [
-    'status' => 'success',
-    'message' => 'Works',
+    'status' => $status,
+    'message' => $message,
     'waited for' => $timemessage
 ];
 http_response_code(200);
