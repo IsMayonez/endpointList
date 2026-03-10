fe6b74dc03c22a7641b08679a0a5d64cd0808be8
Aiman El Sewisy <aiman.sewisy@gmail.com>
2026-03-10 11:32:21 +0100

    minfactor maxfactor

--- exampleEndpoint.php
+++ exampleEndpoint.php
@@ -6,12 +6,16 @@ header('Content-Type: application/json');
 if(isset($_GET['arg1']) && isset($_GET['arg2'])) {
     $param1 = $_GET['arg1'];
     $param2 = $_GET['arg2'];
+    $minfactor = 0.5;
+    $maxfactor = 1.5;
 
     $combinedString = $param1 . ' ' . $param2;
     $timemessage = 0;
     if(isset($_GET['duration'])) {
-        sleep((int)$_GET['duration']);
-        $timemessage = $_GET['duration'];
+        $factor = $minfactor + mt_rand() / mt_getrandmax() * ($maxfactor - $minfactor);
+        $duration = (float)$_GET['duration'] * $factor;
+        usleep((int)($duration * 1000000));
+        $timemessage = $duration;
     }
     
     $response = [
